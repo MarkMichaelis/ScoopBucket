9e2e29f97e4c67309295cb5bb344f497238d0580
Mark Michaelis <mark@IntelliTect.com>
2020-05-31 08:25:19 -0700

    --amend

--- bucket/GitConfigVisualStudio.ps1
+++ bucket/GitConfigVisualStudio.ps1
@@ -14,7 +14,10 @@ Function Invoke-GitConfigVisualStudio {
     }
 
     git config --global mergetool.visual-studio.path "\`"$vsInstallPath\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\vsdiffmerge.exe\`" \`"`$REMOTE\`" \`"`$LOCAL\`" \`"`$BASE\`" \`"`$MERGED\`" //m"
+    git config --global mergetool.visual-studio.keepBackup false
+    git config --global mergetool.visual-studio.trustExitCode true
     git config --global difftool.visual-studio.cmd "\`"$vsInstallPath\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\vsdiffmerge.exe\`" \`"`$LOCAL\`" \`"`$REMOTE\`" //t"
+    git config --global difftool.visual-studio.keepBackup false
 
 }
 Invoke-GitConfigVisualStudio
