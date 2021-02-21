87a928b5add90aee76dd6aacd30b1743dcababbd
Mark Michaelis <mark@IntelliTect.com>
2021-02-20 23:02:37 -0800

    Added configuration for Windows PowerShell and PowerShellCore

---
+++ bucket/PowerShell.ps1
@@ -0,0 +1,16 @@
+
+Write-Host 'Installing and configuring PowerShell...'
+. "$PSScriptRoot\Utils.ps1"
+
+Update-Help
+if((Get-PSRepository PSGallery).InstallationPolicy -ne 'Trusted') {
+    Set-PSRepository -Name PSGallery -InstallationPolicy 'Trusted'
+}
+Install-Module PowershellGet -Repository PSGallery  # Updated to allow support for -AllowPrerelease
+Install-Module Pscx -AllowClobber -AllowPrerelease  # Both Pscx and IntelliTect.File support Edit-File. 
+                                                    # IntelliTect.File will get priority once if it appears first in the PSModulePath
+                                                    # or it is installed after Pscx (if not using source code)
+scoop install Pester
+
+
+
