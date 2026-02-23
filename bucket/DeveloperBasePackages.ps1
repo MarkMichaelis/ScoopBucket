d49907c25b8ab46b5ed222aa62baa86c82306a8e
Mark Michaelis <mark@michaelis.net>
2026-02-22 19:19:21 -0800

    Reorganize packages and disable unused installs
    - Move SoX and eSpeak-NG from OSBasePackages to ClientBasePackages
    - Move WindowsTerminal to the top of OSBasePackages list
    - Comment out Comet (Perplexity) and PowerAutomate in ClientBasePackages
    - Comment out Cursor, AIShell, and Miniforge3 in DeveloperBasePackages
    - Comment out Notepad2-mod in OSBasePackages
    - Remove gh from scoop install list in DeveloperBasePackages

--- bucket/DeveloperBasePackages.ps1
+++ bucket/DeveloperBasePackages.ps1
@@ -8,18 +8,18 @@ Write-Host 'Installing and configuring OSBasePackages...'
 }
 
 #hub - GitHub CLI
-'gh', 'dotnet', 'VisualStudio2026Enterprise' | ForEach-Object { 
+'dotnet', 'VisualStudio2026Enterprise' | ForEach-Object { 
     Write-Host "Installing $_..."
     scoop install -g $_
 }
 
 $WingetPackages = @{
     'VisualStudioCode'=([PSCustomObject]@{ WingetName='Visual Studio Code'; WinGetID='Microsoft.VisualStudioCode'; })
-    'Cursor'=([PSCustomObject]@{ WingetName='Cursor'; WinGetID='Anysphere.Cursor'; })
+#    'Cursor'=([PSCustomObject]@{ WingetName='Cursor'; WinGetID='Anysphere.Cursor'; })
     'CopilotCLI'=([PSCustomObject]@{ WingetName='Copilot CLI'; WinGetID='GitHub.Copilot'; })
-    'AIShell'=([PSCustomObject]@{ WingetName='AI Shell'; WinGetID='Microsoft.AIShell'; })
+#    'AIShell'=([PSCustomObject]@{ WingetName='AI Shell'; WinGetID='Microsoft.AIShell'; })
     'Python'=([PSCustomObject]@{ WingetName='Python'; WinGetID='Python.Python.3.14'; })
-    'Miniforge3'=([PSCustomObject]@{ WingetName='Miniforge3'; WinGetID='CondaForge.Miniforge3'; })
+#    'Miniforge3'=([PSCustomObject]@{ WingetName='Miniforge3'; WinGetID='CondaForge.Miniforge3'; })
     'BeyondCompare'=([PSCustomObject]@{ WingetName='Beyond Compare'; WinGetID='ScooterSoftware.BeyondCompare.4'; })
 }
 
--- bucket/OSBasePackages.ps1
+++ bucket/OSBasePackages.ps1
@@ -3,8 +3,9 @@ Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
 $OSPackages = @{
+    'WindowsTerminal'=([PSCustomObject]@{ WingetName='Windows Terminal'; WinGetID='Microsoft.WindowsTerminal';  })
     '7Zip'=([PSCustomObject]@{ WingetName='7-Zip'; WinGetID='7Zip.7Zip';  })
-    'Notepad2-mod'=([PSCustomObject]@{ WingetName='Notepad2-mod'; WinGetID=' Notepad2mod.Notepad2mod';  })
+#    'Notepad2-mod'=([PSCustomObject]@{ WingetName='Notepad2-mod'; WinGetID=' Notepad2mod.Notepad2mod';  })
     'Everything'=([PSCustomObject]@{ WingetName='Everything'; WinGetID='voidtools.Everything';  })
     'Everything Cli'=([PSCustomObject]@{ WingetName='Everything Cli'; WinGetID='voidtools.Everything.Cli';  })
     'Google Chrome'=([PSCustomObject]@{ WingetName='Google Chrome'; WinGetID='Google.Chrome';  })
@@ -15,10 +16,7 @@ $OSPackages = @{
     'bat'=([PSCustomObject]@{ WingetName='bat'; WinGetID='sharkdp.bat';  }) # Supports Git integration which has not knowingly been configured.
     'BurntSushi.ripgrep.MSVC'=([PSCustomObject]@{ WingetName='Ripgrep'; WinGetID='BurntSushi.ripgrep.MSVC';  }) 
     'fzf'=([PSCustomObject]@{ WingetName='fzf'; WinGetID='junegunn.fzf';  })  # Needs PowerShell install of PSFzf module. See https://github.com/kelleyma49/PSFzf
-    'WindowsTerminal'=([PSCustomObject]@{ WingetName='Windows Terminal'; WinGetID='Microsoft.WindowsTerminal';  })
     'FFmpeg'=([PSCustomObject]@{ WingetName='FFmpeg'; WinGetID='Gyan.FFmpeg';  })
-    'eSpeak-NG'=([PSCustomObject]@{ WingetName='eSpeak NG'; WinGetID='eSpeak-NG.eSpeak-NG';  })
-    'SoX'=([PSCustomObject]@{ WingetName='SoX'; WinGetID='ChrisBagwell.SoX';  })
 }
 
 $OSPackages.VAlues | `
